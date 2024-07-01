171e0d6f8472eb92dec199629a79886769c89aef
Luciferlocas <rohitsomvanshi2003@gmail.com>
2024-07-02 00:43:31 +0530

    removed bug temp

--- src/app/context/ProjectContext.tsx
+++ src/app/context/ProjectContext.tsx
@@ -30,49 +30,49 @@ export const ProjectProvider: React.FC<ProjectProviderProps> = ({
       name: "Tiny URL",
       info: "Github Search enables users to find repositories and developers. Click the link to start searching.",
       link: "https://githubsearch.luciferlocas.live/",
-      vs: "",
-      vb : ""
+      vs: "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4",
+      vb : "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4"
     },
     {
       name: "Edustar",
       info: "Edustar provides educational management solutions. Click the link to learn about its features.",
       link: "https://edustar.luciferlocas.live/",
-      vs: "",
+      vs: "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4",
       vb : "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4"
     },
     {
       name: "Vigilance+",
       info: "Vigilance+ is a project aimed at enhancing security measures. Visit the website for more information.",
       link: "https://vigilance.luciferlocas.live/",
-      vs: "",
+      vs: "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4",
       vb : "https://res.cloudinary.com/dotbo6chr/video/upload/v1719860008/vigilancebig_ugqxzz.mp4"
     },
     {
       name: "Bard Clone",
       info: "Bard Clone is a project that replicates a popular blogging platform. Follow the link to explore more.",
       link: "https://bard.luciferlocas.live/",
-      vs: "",
+      vs: "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4",
       vb : "https://res.cloudinary.com/dotbo6chr/video/upload/v1719860007/Bard_Clonebig_oqdrbo.mp4"
     },
     {
       name: "VidyutKavach",
       info: "VidyutKavach focuses on electricity protection systems. Visit the website to see how it works.",
       link: "https://vidyutkavach.luciferlocas.live/",
-      vs: "",
+      vs: "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4",
       vb : "https://res.cloudinary.com/dotbo6chr/video/upload/v1719860009/VidyutKavachbig_efyg7f.mp4"
     },
     {
       name: "Github Search",
       info: "Github Search enables users to find repositories and developers. Click the link to start searching.",
       link: "https://githubsearch.luciferlocas.live/",
-      vs: "",
+      vs: "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4",
       vb : "https://res.cloudinary.com/dotbo6chr/video/upload/v1719860007/Github_Searchbig_bryxjn.mp4"
     },
     {
       name: "Writer's Oasis",
       info: "Writer's Oasis offers a serene writing environment. Explore more about its features and benefits.",
       link: "https://writersoasis.luciferlocas.live/",
-      vs: "",
+      vs: "https://res.cloudinary.com/dotbo6chr/video/upload/v1719858974/edustar_nhnaja.mp4",
       vb : "https://res.cloudinary.com/dotbo6chr/video/upload/v1719857759/Writer_sOasisbig_rcokif.mp4"
     },
   ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import AboutProject from "./component/AboutProject";
 const page = () => {
   return (
     <>
-      <div className="min-h-screen flex flex-col gap-[3rem] sm:w-9/12 w-10/12 mx-auto pt-[4em] pb-[7em]">
+      <div className="min-h-screen flex flex-col gap-[3rem] sm:w-9/12 w-11/12 mx-auto pt-[4em] pb-[7em]">
         <Nav />
         <Me />
         <ProjectList />
